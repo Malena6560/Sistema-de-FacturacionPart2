08ac5350d64f72ecb122bb308152293280a5c2d9
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-07 18:20:50 -0400

    Sistema de Facturacion
    Creacion de la clase proveedores

---
+++ SistemaFactura2/SistemaFactura2/Models/Proveedores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFactura2.Models
+{
+    public class Proveedores
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int IDProveedores { get; set; }
+        [Required]
+        public int RNC { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
