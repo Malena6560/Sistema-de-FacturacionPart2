156a2c8cea2a6e859b0788ce32f6261292e176ab
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-07 19:03:31 -0400

    Sistema de Facturacion
    Creacion de la base de datos y el controlador clientes que usa vista de entity framework y sus metodos.

---
+++ SistemaFactura2/SistemaFactura2/Migrations/202004072254305_v1.cs
@@ -0,0 +1,131 @@
+﻿namespace SistemaFactura2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categorias",
+                c => new
+                    {
+                        IDCategoria = c.Int(nullable: false, identity: true),
+                        Categoria = c.String(),
+                    })
+                .PrimaryKey(t => t.IDCategoria);
+            
+            CreateTable(
+                "dbo.Clientes",
+                c => new
+                    {
+                        IDClientes = c.Int(nullable: false, identity: true),
+                        RNC = c.Int(nullable: false),
+                        Nombre = c.String(nullable: false),
+                        Telefono = c.String(nullable: false),
+                        Email = c.String(),
+                        IDCategoria = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IDClientes)
+                .ForeignKey("dbo.Categorias", t => t.IDCategoria, cascadeDelete: true)
+                .Index(t => t.IDCategoria);
+            
+            CreateTable(
+                "dbo.DetalleFacturas",
+                c => new
+                    {
+                        IDDetalle = c.Int(nullable: false, identity: true),
+                        IDFactura = c.Int(nullable: false),
+                        IDProducto = c.Int(nullable: false),
+                        Cantidad = c.Int(nullable: false),
+                        Precio = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        SubTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.IDDetalle)
+                .ForeignKey("dbo.Facturas", t => t.IDFactura, cascadeDelete: true)
+                .ForeignKey("dbo.Productos", t => t.IDProducto, cascadeDelete: true)
+                .Index(t => t.IDFactura)
+                .Index(t => t.IDProducto);
+            
+            CreateTable(
+                "dbo.Facturas",
+                c => new
+                    {
+                        IDFactura = c.Int(nullable: false, identity: true),
+                        IDCliente = c.Int(nullable: false),
+                        SubTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Descuento = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ITBIS = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        FechaFactura = c.DateTime(nullable: false),
+                        FechaCreacion = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.IDFactura)
+                .ForeignKey("dbo.Clientes", t => t.IDCliente, cascadeDelete: true)
+                .Index(t => t.IDCliente);
+            
+            CreateTable(
+                "dbo.Productos",
+                c => new
+                    {
+                        IDProductos = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(nullable: false),
+                        Precio = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.IDProductos);
+            
+            CreateTable(
+                "dbo.EntradaMercancias",
+                c => new
+                    {
+                        IDEntrada = c.Int(nullable: false, identity: true),
+                        IDProducto = c.Int(nullable: false),
+                        Cantidad = c.Int(nullable: false),
+                        IDProveedor = c.Int(nullable: false),
+                        FechaEntrada = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.IDEntrada)
+                .ForeignKey("dbo.Productos", t => t.IDProducto, cascadeDelete: true)
+                .ForeignKey("dbo.Proveedores", t => t.IDProveedor, cascadeDelete: true)
+                .Index(t => t.IDProducto)
+                .Index(t => t.IDProveedor);
+            
+            CreateTable(
+                "dbo.Proveedores",
+                c => new
+                    {
+                        IDProveedores = c.Int(nullable: false, identity: true),
+                        RNC = c.Int(nullable: false),
+                        Nombre = c.String(nullable: false),
+                        Telefono = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.IDProveedores);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.EntradaMercancias", "IDProveedor", "dbo.Proveedores");
+            DropForeignKey("dbo.EntradaMercancias", "IDProducto", "dbo.Productos");
+            DropForeignKey("dbo.DetalleFacturas", "IDProducto", "dbo.Productos");
+            DropForeignKey("dbo.DetalleFacturas", "IDFactura", "dbo.Facturas");
+            DropForeignKey("dbo.Facturas", "IDCliente", "dbo.Clientes");
+            DropForeignKey("dbo.Clientes", "IDCategoria", "dbo.Categorias");
+            DropIndex("dbo.EntradaMercancias", new[] { "IDProveedor" });
+            DropIndex("dbo.EntradaMercancias", new[] { "IDProducto" });
+            DropIndex("dbo.Facturas", new[] { "IDCliente" });
+            DropIndex("dbo.DetalleFacturas", new[] { "IDProducto" });
+            DropIndex("dbo.DetalleFacturas", new[] { "IDFactura" });
+            DropIndex("dbo.Clientes", new[] { "IDCategoria" });
+            DropTable("dbo.Proveedores");
+            DropTable("dbo.EntradaMercancias");
+            DropTable("dbo.Productos");
+            DropTable("dbo.Facturas");
+            DropTable("dbo.DetalleFacturas");
+            DropTable("dbo.Clientes");
+            DropTable("dbo.Categorias");
+        }
+    }
+}
---
+++ SistemaFactura2/SistemaFactura2/Models/MantenimientoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SistemaFactura2.Models
+{
+    public class MantenimientoContext:DbContext
+    {
+        public MantenimientoContext() : base("FinalSistemaF")
+        {
+
+        }
+
+        public DbSet<Productos> Producto { get; set; }
+        public DbSet<Clientes> Cliente { get; set; }
+        public DbSet<Proveedores> Proveedor { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<DetalleFactura> DetalleFacturas { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<EntradaMercancia> EntradaMercancias { get; set; }
+    }
+}
