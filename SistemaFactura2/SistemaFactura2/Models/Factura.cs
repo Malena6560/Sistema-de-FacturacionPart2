279fd3e2c789386f5d72a1427c22f6a7ce29b33e
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-07 18:24:25 -0400

    Sistema de Facturacion
    Creacion de la clase Factura con sus repectivas restricciones

---
+++ SistemaFactura2/SistemaFactura2/Models/Factura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFactura2.Models
+{
+    public class Factura
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int IDFactura { get; set; }
+
+        public int IDCliente { get; set; }
+
+        [ForeignKey("IDCliente")]
+        public Clientes Cliente { get; set; }
+
+        public decimal SubTotal { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal ITBIS { get; set; }
+        public decimal Total { get; set; }
+        [Required]
+        public DateTime FechaFactura { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
