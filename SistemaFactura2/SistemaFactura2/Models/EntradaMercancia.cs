c9023178a83baf20ee98ac736daacbc5cfc0c4ed
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-07 18:40:47 -0400

    Sistema de Facturacion
    Creacion de la clase entradas mercancias con sus repectivas restricciones

---
+++ SistemaFactura2/SistemaFactura2/Models/EntradaMercancia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFactura2.Models
+{
+    public class EntradaMercancia
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int IDEntrada { get; set; }
+        public int IDProducto { get; set; }
+
+        [ForeignKey("IDProducto")]
+        public Productos Producto { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+        public int IDProveedor { get; set; }
+
+        [ForeignKey("IDProveedor")]
+        public Proveedores Proveedor { get; set; }
+
+        [Required]
+        public DateTime FechaEntrada { get; set; }
+    }
+}
