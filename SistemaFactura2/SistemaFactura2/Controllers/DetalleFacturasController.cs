d43ea78e821798ddb7391803c28a39189c0e5a0f
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-07 19:39:08 -0400

    Sistema de Facturacion
    Controladores de Facturas, Detalle de facturas y proveedores, creados y funcionando los cuales utilizan vistas que usan entity framework.

---
+++ SistemaFactura2/SistemaFactura2/Controllers/DetalleFacturasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaFactura2.Models;
+
+namespace SistemaFactura2.Controllers
+{
+    public class DetalleFacturasController : Controller
+    {
+        private MantenimientoContext db = new MantenimientoContext();
+
+        // GET: DetalleFacturas
+        public ActionResult Index()
+        {
+            var detalleFacturas = db.DetalleFacturas.Include(d => d.Factura).Include(d => d.Producto);
+            return View(detalleFacturas.ToList());
+        }
+
+        // GET: DetalleFacturas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            if (detalleFactura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleFactura);
+        }
+
+        // GET: DetalleFacturas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDFactura = new SelectList(db.Facturas, "IDFactura", "IDFactura");
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre");
+            return View();
+        }
+
+        // POST: DetalleFacturas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDDetalle,IDFactura,IDProducto,Cantidad,Precio,SubTotal")] DetalleFactura detalleFactura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetalleFacturas.Add(detalleFactura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDFactura = new SelectList(db.Facturas, "IDFactura", "IDFactura", detalleFactura.IDFactura);
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre", detalleFactura.IDProducto);
+            return View(detalleFactura);
+        }
+
+        // GET: DetalleFacturas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            if (detalleFactura == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDFactura = new SelectList(db.Facturas, "IDFactura", "IDFactura", detalleFactura.IDFactura);
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre", detalleFactura.IDProducto);
+            return View(detalleFactura);
+        }
+
+        // POST: DetalleFacturas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDDetalle,IDFactura,IDProducto,Cantidad,Precio,SubTotal")] DetalleFactura detalleFactura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detalleFactura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDFactura = new SelectList(db.Facturas, "IDFactura", "IDFactura", detalleFactura.IDFactura);
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre", detalleFactura.IDProducto);
+            return View(detalleFactura);
+        }
+
+        // GET: DetalleFacturas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            if (detalleFactura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleFactura);
+        }
+
+        // POST: DetalleFacturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            db.DetalleFacturas.Remove(detalleFactura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SistemaFactura2/SistemaFactura2/Controllers/EntradaMercanciasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaFactura2.Models;
+
+namespace SistemaFactura2.Controllers
+{
+    public class EntradaMercanciasController : Controller
+    {
+        private MantenimientoContext db = new MantenimientoContext();
+
+        // GET: EntradaMercancias
+        public ActionResult Index()
+        {
+            var entradaMercancias = db.EntradaMercancias.Include(e => e.Producto).Include(e => e.Proveedor);
+            return View(entradaMercancias.ToList());
+        }
+
+        // GET: EntradaMercancias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
+            if (entradaMercancia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entradaMercancia);
+        }
+
+        // GET: EntradaMercancias/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre");
+            ViewBag.IDProveedor = new SelectList(db.Proveedor, "IDProveedores", "Nombre");
+            return View();
+        }
+
+        // POST: EntradaMercancias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDEntrada,IDProducto,Cantidad,IDProveedor,FechaEntrada")] EntradaMercancia entradaMercancia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EntradaMercancias.Add(entradaMercancia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre", entradaMercancia.IDProducto);
+            ViewBag.IDProveedor = new SelectList(db.Proveedor, "IDProveedores", "Nombre", entradaMercancia.IDProveedor);
+            return View(entradaMercancia);
+        }
+
+        // GET: EntradaMercancias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
+            if (entradaMercancia == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre", entradaMercancia.IDProducto);
+            ViewBag.IDProveedor = new SelectList(db.Proveedor, "IDProveedores", "Nombre", entradaMercancia.IDProveedor);
+            return View(entradaMercancia);
+        }
+
+        // POST: EntradaMercancias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDEntrada,IDProducto,Cantidad,IDProveedor,FechaEntrada")] EntradaMercancia entradaMercancia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entradaMercancia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDProducto = new SelectList(db.Producto, "IDProductos", "Nombre", entradaMercancia.IDProducto);
+            ViewBag.IDProveedor = new SelectList(db.Proveedor, "IDProveedores", "Nombre", entradaMercancia.IDProveedor);
+            return View(entradaMercancia);
+        }
+
+        // GET: EntradaMercancias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
+            if (entradaMercancia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entradaMercancia);
+        }
+
+        // POST: EntradaMercancias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EntradaMercancia entradaMercancia = db.EntradaMercancias.Find(id);
+            db.EntradaMercancias.Remove(entradaMercancia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
