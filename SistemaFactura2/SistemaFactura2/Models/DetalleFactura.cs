5d402a820c7c5cd1e9ebf0d0d92816b3f0fa0682
Malena Colon Gomez <colongomezmalena@gmail.com>
2020-04-07 18:35:05 -0400

    Sistema de Facturacion
    Creacion de la clase Detalle de Factura con sus debidas restricciones

---
+++ SistemaFactura2/SistemaFactura2/Models/DetalleFactura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFactura2.Models
+{
+    public class DetalleFactura
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int IDDetalle { get; set; }
+
+        public int IDFactura { get; set; }
+
+        [ForeignKey("IDFactura")]
+        public Factura Factura { get; set; }
+
+        public int IDProducto { get; set; }
+
+        [ForeignKey("IDProducto")]
+        public Productos Producto { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public decimal Precio { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
